6528ca222a201da9a7b62f4c84170cb407b3fe9e
vanessa-klein-24122022 <165963445+vanessa-klein-24122022@users.noreply.github.com>
2024-09-18 19:43:56 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -27,3 +27,43 @@ criaCartao(
     'Isso é um teste?',
     'SIM'
 )
+
+criacartao(
+    'conhecimentos gerais'
+    'de quem é a famosa frase "peno logo existo'
+    'Descartes "Je pense, donc Je suis"
+)
+
+criacartao(
+    'américa latina'
+    'qual a nacionaliade de Che Guevara'
+    'argentina'
+)
+
+criacartao(
+    'america do sul'
+    'qual o maior país da america do sul'
+    'Brasil
+)
+
+criacartao(
+    'conhecimento gerais'
+    'Quantas linhas tem no logo do serviço de streaming de música Spotify?'
+    '3 linhas'
+)
+
+criacatao(
+    'Brasil'
+    'Qual inventor brasileiro é conhecido com o “pai da aviação”?'
+    'Santos Dumont'
+)
+criacartao(
+    'culinária'
+    'Qual dos doces abaixo é tipicamente brasileiro?'
+    'brigadeiro'
+)
+criacartao(
+    'história'
+    'Qual evento histórico aconteceu entre os anos de 1914 e 1918?'
+    'primeira guerra mundial'
+)
